0e0cb19ce775d4ef0d3b9aa953bdfdab2c1eaaf4
Jaqobs <QuanSushi@users.noreply.github.com>
2015-06-30 13:50:06 +0200

    Update scripts.js
    redone with new dmg formular by Daniel Görlitz and pinopi

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,19 +2,26 @@ $(document).ready( function calculate() {
 	$('#start').click( function() {
 		$(this).fadeTo(0, 0.6);
 		$('#totalMultiplier').empty();
-		var cpt = $('input[name=captain]').val();
-		var fr_cpt = $('input[name=fr_captain]').val();
-		var multiplier = (cpt*fr_cpt).toFixed(2);
-		var ship = $('input[name=ship]').val();
+		
+		var cpt = $('select[name=captain]').val();
+		var fr_cpt = $('select[name=fr_captain]').val();
+		var ship = $('select[name=ship]').val();
+		var type = $('select[name=type]').val();
+		var orb = $('select[name=orb]').val();
+		var combo = $('select[name=combo]').val();
 		var perfect = 1.9;
-		$('#totalMultiplier').append(multiplier);
+		var multiplier = (cpt*fr_cpt*ship*type*orb);
+		
+		$('#totalMultiplier').append((multiplier).toFixed(2));
 
-		$('#1st_value').text((20000/multiplier/ship/perfect).toFixed(0));
-		$('#2nd_value').text((20000/multiplier/ship/1.3/perfect).toFixed(0));
-		$('#3rd_value').text((20000/multiplier/ship/1.6/perfect).toFixed(0));
-		$('#4th_value').text((20000/multiplier/ship/1.9/perfect).toFixed(0));
-		$('#5th_value').text((20000/multiplier/ship/2.2/perfect).toFixed(0));
-		$('#6th_value').text((20000/multiplier/ship/2.5/perfect).toFixed(0));
+		var turtle = $('select[name=turtle]').val();
+
+		$('#1st_value').text((turtle/(0.9*multiplier*1 + multiplier*1/combo)).toFixed(0));
+		$('#2nd_value').text((turtle/(0.9*multiplier*1.3 + multiplier*1.3/combo)).toFixed(0));
+		$('#3rd_value').text((turtle/(0.9*multiplier*1.6 + multiplier*1.6/combo)).toFixed(0));
+		$('#4th_value').text((turtle/(0.9*multiplier*1.9 + multiplier*1.9/combo)).toFixed(0));
+		$('#5th_value').text((turtle/(0.9*multiplier*2.2 + multiplier*2.2/combo)).toFixed(0));
+		$('#6th_value').text((turtle/(0.9*multiplier*2.5 + multiplier*2.5/combo)).toFixed(0));
 	});
 
 	
